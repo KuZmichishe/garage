aed7599bfc10df04fab2df20e651d1763b5284a7
Dmitriy Ermakov <neil.ermak@gmail.com>
2018-02-06 00:06:59 +0200

    Trying to request kuzmich detect_motion URL via Python script #2

--- hardware/test.py
+++ hardware/test.py
@@ -2,7 +2,7 @@ import urllib2
 import time
 
 while True:
-    response = urllib2.urlopen('http://kuzmich.xyz:8000/check_motion/')
+    response = urllib2.urlopen('http://garage.kuzmich.xyz:8000/check_motion/')
     html = response.read()
     print(html)
     time.sleep(2)
